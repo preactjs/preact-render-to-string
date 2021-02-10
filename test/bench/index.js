2be162f497cc9b996ad92aac30400b52a9690775
Jason Miller <jason@developit.ca>
2021-02-10 11:26:24 -0500

    benchmark old VS new

--- test/bench/index.js
+++ test/bench/index.js
@@ -1,28 +1,34 @@
 import { h } from 'preact';
 import { describe, test, expect, benchmark } from './runner.js';
-import renderToString from '../../src/ropes.js';
+import renderToString from '../../src/index.js';
+import renderToStringFast from '../../src/ropes.js';
 import TextApp from './fixtures/text.js';
 import StackApp from './fixtures/stack.js';
 
 describe('performance', () => {
 	test('text', () => {
 		const html = renderToString(h(TextApp));
-		print(html.length + '\n\n');
-		const { hz } = benchmark('text', () => {
-			if (renderToString(h(TextApp)) !== html) {
-				throw Error('mismatch');
-			}
+		const html2 = renderToStringFast(h(TextApp));
+		expect(html.length).toEqual(html2.length);
+		const before = benchmark('before', () => {
+			if (renderToString(h(TextApp)) !== html) throw Error('mismatch');
 		});
-		expect(hz).toBeGreaterThan(20);
+		const fast = benchmark('faster', () => {
+			if (renderToStringFast(h(TextApp)) !== html) throw Error('mismatch');
+		});
+		expect(fast.hz).toBeGreaterThan(before.hz);
 	});
 
 	test('stack', () => {
 		const html = renderToString(h(StackApp));
-		const { hz } = benchmark('stack', () => {
-			if (renderToString(h(StackApp)) !== html) {
-				throw Error('mismatch');
-			}
+		const html2 = renderToStringFast(h(StackApp));
+		expect(html.length).toEqual(html2.length);
+		const before = benchmark('before', () => {
+			if (renderToString(h(StackApp)) !== html) throw Error('mismatch');
 		});
-		expect(hz).toBeGreaterThan(150);
+		const fast = benchmark('faster', () => {
+			if (renderToStringFast(h(StackApp)) !== html) throw Error('mismatch');
+		});
+		expect(fast.hz).toBeGreaterThan(before.hz);
 	});
 });
