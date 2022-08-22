692b92b98d90c6c52bec1cac325be020df8bb02a
Jason Miller <jason.miller@shopify.com>
2022-08-21 23:36:24 -0400

    fix root copy of jsx types

--- jsx.d.ts
+++ jsx.d.ts
@@ -1,13 +1,21 @@
 import { VNode } from 'preact';
 
 interface Options {
-  jsx?: boolean;
-  xml?: boolean;
-  functions?: boolean
-  functionNames?: boolean,
-  skipFalseAttributes?: boolean
-  pretty?: boolean | string;
+	jsx?: boolean;
+	xml?: boolean;
+	pretty?: boolean | string;
+	shallow?: boolean;
+	functions?: boolean;
+	functionNames?: boolean;
+	skipFalseAttributes?: boolean;
 }
 
-export function render(vnode: VNode, context?: any, options?: Options):string;
+export default function renderToStringPretty(
+	vnode: VNode,
+	context?: any,
+	options?: Options
+): string;
+
+export function shallowRender(vnode: VNode, context?: any): string;
+
 export default render;
