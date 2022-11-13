36e07070331c591ba86c730cd3696853fee4c0a7
Jason Miller <jason@developit.ca>
2022-11-13 15:51:55 -0500

    update pretty implementation and move typedefs into a d.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,28 +1,3 @@
 import { VNode } from 'preact';
 
-interface Options {
-	shallow?: boolean;
-	xml?: boolean;
-	pretty?: boolean | string;
-}
-
-export function render(vnode: VNode, context?: any, options?: Options): string;
-export function renderToString(
-	vnode: VNode,
-	context?: any,
-	options?: Options
-): string;
-export function shallowRender(vnode: VNode, context?: any): string;
-
-export interface ChunkedOptions {
-	onError(error: unknown): void;
-	onWrite(chunk: string): void;
-	context?: any;
-	abortSignal?: AbortSignal;
-}
-export function renderToChunks(
-	vnode: VNode,
-	options: ChunkedOptions
-): Promise<void>;
-
-export default render;
+export default function renderToString(vnode: VNode, context?: any): string;
