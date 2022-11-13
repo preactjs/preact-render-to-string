10331d963368e10eb09c39d4207a8fb1bff6ba4c
Jason Miller <jason@developit.ca>
2022-11-13 11:26:00 -0500

    update benchmarking reference implmementation to 5.2.6 (6a0bec2)

--- benchmarks/lib/render-to-string.js
+++ benchmarks/lib/render-to-string.js
@@ -1,12 +1,12 @@
 /* eslint-disable */
-// preact-render-to-string@5.1.22 (7cdf4d6)
-import { Fragment as e, options as t } from 'preact';
-var r = /acit|ex(?:s|g|n|p|$)|rph|grid|ows|mnc|ntw|ine[ch]|zoo|^ord|^--/i,
-	n = /^(area|base|br|col|embed|hr|img|input|link|meta|param|source|track|wbr)$/,
-	o = /[\s\n\\/='"\0<>]/,
-	i = /^xlink:?./,
+// preact-render-to-string@5.2.6 (6a0bec2)
+import { Fragment as e, options as t, h as r } from 'preact';
+var n = /acit|ex(?:s|g|n|p|$)|rph|grid|ows|mnc|ntw|ine[ch]|zoo|^ord|^--/i,
+	o = /^(area|base|br|col|embed|hr|img|input|link|meta|param|source|track|wbr)$/,
+	i = /[\s\n\\/='"\0<>]/,
+	l = /^xlink:?./,
 	a = /["&<]/;
-function l(e) {
+function s(e) {
 	if (!1 === a.test((e += ''))) return e;
 	for (var t = 0, r = 0, n = '', o = ''; r < e.length; r++) {
 		switch (e.charCodeAt(r)) {
@@ -26,10 +26,10 @@ function l(e) {
 	}
 	return r !== t && (n += e.slice(t, r)), n;
 }
-var s = function (e, t) {
+var f = function (e, t) {
 		return String(e).replace(/(\n+)/g, '$1' + (t || '\t'));
 	},
-	f = function (e, t, r) {
+	u = function (e, t, r) {
 		return (
 			String(e).length > (t || 40) ||
 			(!r && -1 !== String(e).indexOf('\n')) ||
@@ -37,76 +37,77 @@ var s = function (e, t) {
 		);
 	},
 	c = {},
-	u = /([A-Z])/g;
+	_ = /([A-Z])/g;
 function p(e) {
 	var t = '';
-	for (var n in e) {
-		var o = e[n];
+	for (var r in e) {
+		var o = e[r];
 		null != o &&
 			'' !== o &&
 			(t && (t += ' '),
 			(t +=
-				'-' == n[0] ? n : c[n] || (c[n] = n.replace(u, '-$1').toLowerCase())),
+				'-' == r[0] ? r : c[r] || (c[r] = r.replace(_, '-$1').toLowerCase())),
 			(t =
-				'number' == typeof o && !1 === r.test(n)
+				'number' == typeof o && !1 === n.test(r)
 					? t + ': ' + o + 'px;'
 					: t + ': ' + o + ';'));
 	}
 	return t || void 0;
 }
-function _(e, t) {
+function d(e, t) {
 	return (
-		Array.isArray(t) ? t.reduce(_, e) : null != t && !1 !== t && e.push(t), e
+		Array.isArray(t) ? t.reduce(d, e) : null != t && !1 !== t && e.push(t), e
 	);
 }
-function d() {
+function v() {
 	this.__d = !0;
 }
-function v(e, t) {
+function h(e, t) {
 	return {
 		__v: e,
 		context: t,
 		props: e.props,
-		setState: d,
-		forceUpdate: d,
+		setState: v,
+		forceUpdate: v,
 		__d: !0,
 		__h: []
 	};
 }
-function h(e, t) {
+function g(e, t) {
 	var r = e.contextType,
 		n = r && t[r.__c];
 	return null != r ? (n ? n.props.value : r.__) : t;
 }
-var g = [];
-function y(r, a, c, u, d, m) {
+var y = [];
+function m(r, n, a, c, _, v) {
 	if (null == r || 'boolean' == typeof r) return '';
-	if ('object' != typeof r) return l(r);
-	var b = c.pretty,
+	if ('object' != typeof r) return 'function' == typeof r ? '' : s(r);
+	var b = a.pretty,
 		x = b && 'string' == typeof b ? b : '\t';
 	if (Array.isArray(r)) {
 		for (var k = '', S = 0; S < r.length; S++)
-			b && S > 0 && (k += '\n'), (k += y(r[S], a, c, u, d, m));
+			b && S > 0 && (k += '\n'), (k += m(r[S], n, a, c, _, v));
 		return k;
 	}
+	if (void 0 !== r.constructor) return '';
 	var w,
 		C = r.type,
 		O = r.props,
 		j = !1;
 	if ('function' == typeof C) {
-		if (((j = !0), !c.shallow || (!u && !1 !== c.renderRootComponent))) {
+		if (((j = !0), !a.shallow || (!c && !1 !== a.renderRootComponent))) {
 			if (C === e) {
 				var A = [];
 				return (
-					_(A, r.props.children), y(A, a, c, !1 !== c.shallowHighOrder, d, m)
+					d(A, r.props.children), m(A, n, a, !1 !== a.shallowHighOrder, _, v)
 				);
 			}
 			var F,
-				H = (r.__c = v(r, a));
+				H = (r.__c = h(r, n));
 			t.__b && t.__b(r);
 			var M = t.__r;
 			if (C.prototype && 'function' == typeof C.prototype.render) {
-				var L = h(C, a);
+				var L = g(C, n);
 				((H = r.__c = new C(O, L)).__v = r),
 					(H._dirty = H.__d = !0),
 					(H.props = O),
@@ -132,12 +133,12 @@ function y(r, a, c, u, d, m) {
 					M && M(r),
 					(F = H.render(H.props, H.state, H.context));
 			} else
-				for (var T = h(C, a), E = 0; H.__d && E++ < 25; )
+				for (var T = g(C, n), E = 0; H.__d && E++ < 25; )
 					(H.__d = !1), M && M(r), (F = C.call(r.__c, O, T));
 			return (
-				H.getChildContext && (a = Object.assign({}, a, H.getChildContext())),
+				H.getChildContext && (n = Object.assign({}, n, H.getChildContext())),
 				t.diffed && t.diffed(r),
-				y(F, a, c, !1 !== c.shallowHighOrder, d, m)
+				m(F, n, a, !1 !== a.shallowHighOrder, _, v)
 			);
 		}
 		C =
@@ -148,12 +149,12 @@ function y(r, a, c, u, d, m) {
 					.call(e)
 					.match(/^\s*function\s+([^( ]+)/) || '')[1];
 				if (!t) {
-					for (var r = -1, n = g.length; n--; )
-						if (g[n] === e) {
+					for (var r = -1, n = y.length; n--; )
+						if (y[n] === e) {
 							r = n;
 							break;
 						}
-					r < 0 && (r = g.push(e) - 1), (t = 'UnnamedComponent' + r);
+					r < 0 && (r = y.push(e) - 1), (t = 'UnnamedComponent' + r);
 				}
 				return t;
 			})(w);
@@ -163,14 +164,14 @@ function y(r, a, c, u, d, m) {
 		N = '<' + C;
 	if (O) {
 		var P = Object.keys(O);
-		c && !0 === c.sortAttributes && P.sort();
+		a && !0 === a.sortAttributes && P.sort();
 		for (var W = 0; W < P.length; W++) {
 			var I = P[W],
 				R = O[I];
 			if ('children' !== I) {
 				if (
-					!o.test(I) &&
-					((c && c.allAttributes) ||
+					!i.test(I) &&
+					((a && a.allAttributes) ||
 						('key' !== I && 'ref' !== I && '__self' !== I && '__source' !== I))
 				) {
 					if ('defaultValue' === I) I = 'value';
@@ -180,8 +181,8 @@ function y(r, a, c, u, d, m) {
 						if (void 0 !== O.class) continue;
 						I = 'class';
 					} else
-						d &&
-							i.test(I) &&
+						_ &&
+							l.test(I) &&
 							(I = I.toLowerCase().replace(/^xlink:?/, 'xlink:'));
 					if ('htmlFor' === I) {
 						if (O.for) continue;
@@ -192,26 +193,26 @@ function y(r, a, c, u, d, m) {
 							'r' === I[1] &&
 							'boolean' == typeof R &&
 							(R = String(R));
-					var U = c.attributeHook && c.attributeHook(I, R, a, c, j);
+					var U = a.attributeHook && a.attributeHook(I, R, n, a, j);
 					if (U || '' === U) N += U;
 					else if ('dangerouslySetInnerHTML' === I) D = R && R.__html;
 					else if ('textarea' === C && 'value' === I) $ = R;
 					else if ((R || 0 === R || '' === R) && 'function' != typeof R) {
-						if (!((!0 !== R && '' !== R) || ((R = I), c && c.xml))) {
+						if (!((!0 !== R && '' !== R) || ((R = I), a && a.xml))) {
 							N = N + ' ' + I;
 							continue;
 						}
 						if ('value' === I) {
 							if ('select' === C) {
-								m = R;
+								v = R;
 								continue;
 							}
 							'option' === C &&
-								m == R &&
+								v == R &&
 								void 0 === O.selected &&
 								(N += ' selected');
 						}
-						N = N + ' ' + I + '="' + l(R) + '"';
+						N = N + ' ' + I + '="' + s(R) + '"';
 					}
 				}
 			} else $ = R;
@@ -223,28 +224,28 @@ function y(r, a, c, u, d, m) {
 			? b && ~N.indexOf('\n') && (N += '\n')
 			: (N = V);
 	}
-	if (((N += '>'), o.test(C)))
+	if (((N += '>'), i.test(C)))
 		throw new Error(C + ' is not a valid HTML tag name in ' + N);
 	var q,
-		z = n.test(C) || (c.voidElements && c.voidElements.test(C)),
+		z = o.test(C) || (a.voidElements && a.voidElements.test(C)),
 		Z = [];
-	if (D) b && f(D) && (D = '\n' + x + s(D, x)), (N += D);
-	else if (null != $ && _((q = []), $).length) {
+	if (D) b && u(D) && (D = '\n' + x + f(D, x)), (N += D);
+	else if (null != $ && d((q = []), $).length) {
 		for (var B = b && ~N.indexOf('\n'), G = !1, J = 0; J < q.length; J++) {
 			var K = q[J];
 			if (null != K && !1 !== K) {
-				var Q = y(K, a, c, !0, 'svg' === C || ('foreignObject' !== C && d), m);
-				if ((b && !B && f(Q) && (B = !0), Q))
+				var Q = m(K, n, a, !0, 'svg' === C || ('foreignObject' !== C && _), v);
+				if ((b && !B && u(Q) && (B = !0), Q))
 					if (b) {
 						var X = Q.length > 0 && '<' != Q[0];
 						G && X ? (Z[Z.length - 1] += Q) : Z.push(Q), (G = X);
 					} else Z.push(Q);
 			}
 		}
-		if (b && B) for (var Y = Z.length; Y--; ) Z[Y] = '\n' + x + s(Z[Y], x);
+		if (b && B) for (var Y = Z.length; Y--; ) Z[Y] = '\n' + x + f(Z[Y], x);
 	}
 	if (Z.length || D) N += Z.join('');
-	else if (c && c.xml) return N.substring(0, N.length - 1) + ' />';
+	else if (a && a.xml) return N.substring(0, N.length - 1) + ' />';
 	return (
 		!z || q || D
 			? (b && ~N.indexOf('\n') && (N += '\n'), (N = N + '</' + C + '>'))
@@ -252,36 +253,45 @@ function y(r, a, c, u, d, m) {
 		N
 	);
 }
-var m = { shallow: !0 };
-k.render = k;
-var b = function (e, t) {
-		return k(e, t, m);
+var b = { shallow: !0 };
+S.render = S;
+var x = function (e, t) {
+		return S(e, t, b);
 	},
-	x = [];
-function k(e, r, n) {
-	r = r || {};
-	var o,
-		i = t.__s;
+	k = [];
+function S(n, o, i) {
+	o = o || {};
+	var l = t.__s;
+	t.__s = !0;
+	var a,
+		s = r(e, null);
 	return (
-		(t.__s = !0),
-		(o =
-			n &&
-			(n.pretty ||
-				n.voidElements ||
-				n.sortAttributes ||
-				n.shallow ||
-				n.allAttributes ||
-				n.xml ||
-				n.attributeHook)
-				? y(e, r, n)
-				: j(e, r, !1, void 0)),
-		t.__c && t.__c(e, x),
-		(t.__s = i),
-		(x.length = 0),
-		o
+		(s.__k = [n]),
+		(a =
+			i &&
+			(i.pretty ||
+				i.voidElements ||
+				i.sortAttributes ||
+				i.shallow ||
+				i.allAttributes ||
+				i.xml ||
+				i.attributeHook)
+				? m(n, o, i)
+				: F(n, o, !1, void 0, s)),
+		t.__c && t.__c(n, k),
+		(t.__s = l),
+		(k.length = 0),
+		a
 	);
 }
-function S(e, t) {
+function w(e) {
+	return null == e || 'boolean' == typeof e
+		? null
+		: 'string' == typeof e || 'number' == typeof e || 'bigint' == typeof e
+		? r(null, null, e)
+		: e;
+}
+function C(e, t) {
 	return 'className' === e
 		? 'class'
 		: 'htmlFor' === e
@@ -292,136 +302,153 @@ function S(e, t) {
 		? 'checked'
 		: 'defaultSelected' === e
 		? 'selected'
-		: t && i.test(e)
+		: t && l.test(e)
 		? e.toLowerCase().replace(/^xlink:?/, 'xlink:')
 		: e;
 }
-function w(e, t) {
+function O(e, t) {
 	return 'style' === e && null != t && 'object' == typeof t
 		? p(t)
 		: 'a' === e[0] && 'r' === e[1] && 'boolean' == typeof t
 		? String(t)
 		: t;
 }
-var C = Array.isArray,
-	O = Object.assign;
-function j(r, i, a, s) {
+var j = Array.isArray,
+	A = Object.assign;
+function F(r, n, l, a, f) {
 	if (null == r || !0 === r || !1 === r || '' === r) return '';
-	if ('object' != typeof r) return l(r);
-	if (C(r)) {
-		for (var f = '', c = 0; c < r.length; c++) f += j(r[c], i, a, s);
-		return f;
+	if ('object' != typeof r) return 'function' == typeof r ? '' : s(r);
+	if (j(r)) {
+		var u = '';
+		f.__k = r;
+		for (var c = 0; c < r.length; c++)
+			(u += F(r[c], n, l, a, f)), (r[c] = w(r[c]));
+		return u;
 	}
-	t.__b && t.__b(r);
-	var u = r.type,
+	if (void 0 !== r.constructor) return '';
+	(r.__ = f), t.__b && t.__b(r);
+	var _ = r.type,
 		p = r.props;
-	if ('function' == typeof u) {
-		if (u === e) return j(r.props.children, i, a, s);
-		var _;
-		_ =
-			u.prototype && 'function' == typeof u.prototype.render
-				? (function (e, r) {
-						var n = e.type,
-							o = h(n, r),
-							i = new n(e.props, o);
-						(e.__c = i),
-							(i.__v = e),
-							(i.__d = !0),
-							(i.props = e.props),
-							null == i.state && (i.state = {}),
-							null == i.__s && (i.__s = i.state),
-							(i.context = o),
-							n.getDerivedStateFromProps
-								? (i.state = O(
-										{},
-										i.state,
-										n.getDerivedStateFromProps(i.props, i.state)
-								  ))
-								: i.componentWillMount &&
-								  (i.componentWillMount(),
-								  (i.state = i.__s !== i.state ? i.__s : i.state));
-						var a = t.__r;
-						return a && a(e), i.render(i.props, i.state, i.context);
-				  })(r, i)
-				: (function (e, r) {
-						var n,
-							o = v(e, r),
-							i = h(e.type, r);
-						e.__c = o;
-						for (var a = t.__r, l = 0; o.__d && l++ < 25; )
-							(o.__d = !1), a && a(e), (n = e.type.call(o, e.props, i));
-						return n;
-				  })(r, i);
-		var d = r.__c;
-		d.getChildContext && (i = O({}, i, d.getChildContext()));
-		var g = j(_, i, a, s);
-		return t.diffed && t.diffed(r), g;
+	if ('function' == typeof _) {
+		var d;
+		if (_ === e) d = p.children;
+		else {
+			d =
+				_.prototype && 'function' == typeof _.prototype.render
+					? (function (e, r) {
+							var n = e.type,
+								o = g(n, r),
+								i = new n(e.props, o);
+							(e.__c = i),
+								(i.__v = e),
+								(i.__d = !0),
+								(i.props = e.props),
+								null == i.state && (i.state = {}),
+								null == i.__s && (i.__s = i.state),
+								(i.context = o),
+								n.getDerivedStateFromProps
+									? (i.state = A(
+											{},
+											i.state,
+											n.getDerivedStateFromProps(i.props, i.state)
+									  ))
+									: i.componentWillMount &&
+									  (i.componentWillMount(),
+									  (i.state = i.__s !== i.state ? i.__s : i.state));
+							var l = t.__r;
+							return l && l(e), i.render(i.props, i.state, i.context);
+					  })(r, n)
+					: (function (e, r) {
+							var n,
+								o = h(e, r),
+								i = g(e.type, r);
+							e.__c = o;
+							for (var l = t.__r, a = 0; o.__d && a++ < 25; )
+								(o.__d = !1), l && l(e), (n = e.type.call(o, e.props, i));
+							return n;
+					  })(r, n);
+			var v = r.__c;
+			v.getChildContext && (n = A({}, n, v.getChildContext()));
+		}
+		var y = F(
+			(d = null != d && d.type === e && null == d.key ? d.props.children : d),
+			n,
+			l,
+			a,
+			r
+		);
+		return (
+			t.diffed && t.diffed(r), (r.__ = void 0), t.unmount && t.unmount(r), y
+		);
 	}
-	var y,
-		m,
-		b = '<';
-	if (((b += u), p))
-		for (var x in ((y = p.children), p)) {
-			var k = p[x];
+	var m,
+		b,
+		x = '<';
+	if (((x += _), p))
+		for (var k in ((m = p.children), p)) {
+			var S = p[k];
 			if (
 				!(
-					'key' === x ||
-					'ref' === x ||
-					'__self' === x ||
-					'__source' === x ||
-					'children' === x ||
-					('className' === x && 'class' in p) ||
-					('htmlFor' === x && 'for' in p) ||
-					o.test(x)
+					'key' === k ||
+					'ref' === k ||
+					'__self' === k ||
+					'__source' === k ||
+					'children' === k ||
+					('className' === k && 'class' in p) ||
+					('htmlFor' === k && 'for' in p) ||
+					i.test(k)
 				)
 			)
-				if (((k = w((x = S(x, a)), k)), 'dangerouslySetInnerHTML' === x))
-					m = k && k.__html;
-				else if ('textarea' === u && 'value' === x) y = k;
-				else if ((k || 0 === k || '' === k) && 'function' != typeof k) {
-					if (!0 === k || '' === k) {
-						(k = x), (b = b + ' ' + x);
+				if (((S = O((k = C(k, l)), S)), 'dangerouslySetInnerHTML' === k))
+					b = S && S.__html;
+				else if ('textarea' === _ && 'value' === k) m = S;
+				else if ((S || 0 === S || '' === S) && 'function' != typeof S) {
+					if (!0 === S || '' === S) {
+						(S = k), (x = x + ' ' + k);
 						continue;
 					}
-					if ('value' === x) {
-						if ('select' === u) {
-							s = k;
+					if ('value' === k) {
+						if ('select' === _) {
+							a = S;
 							continue;
 						}
-						'option' !== u || s != k || 'selected' in p || (b += ' selected');
+						'option' !== _ || a != S || 'selected' in p || (x += ' selected');
 					}
-					b = b + ' ' + x + '="' + l(k) + '"';
+					x = x + ' ' + k + '="' + s(S) + '"';
 				}
 		}
-	var A = b;
-	if (((b += '>'), o.test(u)))
-		throw new Error(u + ' is not a valid HTML tag name in ' + b);
-	var F = '',
-		H = !1;
-	if (m) (F += m), (H = !0);
-	else if ('string' == typeof y) (F += l(y)), (H = !0);
-	else if (C(y))
-		for (var M = 0; M < y.length; M++) {
-			var L = y[M];
-			if (null != L && !1 !== L) {
-				var T = j(L, i, 'svg' === u || ('foreignObject' !== u && a), s);
-				T && ((F += T), (H = !0));
+	var H = x;
+	if (((x += '>'), i.test(_)))
+		throw new Error(_ + ' is not a valid HTML tag name in ' + x);
+	var M = '',
+		L = !1;
+	if (b) (M += b), (L = !0);
+	else if ('string' == typeof m) (M += s(m)), (L = !0);
+	else if (j(m)) {
+		r.__k = m;
+		for (var T = 0; T < m.length; T++) {
+			var E = m[T];
+			if (((m[T] = w(E)), null != E && !1 !== E)) {
+				var $ = F(E, n, 'svg' === _ || ('foreignObject' !== _ && l), a, r);
+				$ && ((M += $), (L = !0));
 			}
 		}
-	else if (null != y && !1 !== y && !0 !== y) {
-		var E = j(y, i, 'svg' === u || ('foreignObject' !== u && a), s);
-		E && ((F += E), (H = !0));
+	} else if (null != m && !1 !== m && !0 !== m) {
+		r.__k = [w(m)];
+		var D = F(m, n, 'svg' === _ || ('foreignObject' !== _ && l), a, r);
+		D && ((M += D), (L = !0));
 	}
-	if ((t.diffed && t.diffed(r), H)) b += F;
-	else if (n.test(u)) return A + ' />';
-	return b + '</' + u + '>';
+	if ((t.diffed && t.diffed(r), (r.__ = void 0), t.unmount && t.unmount(r), L))
+		x += M;
+	else if (o.test(_)) return H + ' />';
+	return x + '</' + _ + '>';
 }
-k.shallowRender = b;
-export default k;
+S.shallowRender = x;
+export default S;
 export {
-	k as render,
-	k as renderToStaticMarkup,
-	k as renderToString,
-	b as shallowRender
+	S as render,
+	S as renderToStaticMarkup,
+	S as renderToString,
+	x as shallowRender
 };
 //# sourceMappingURL=index.module.js.map
