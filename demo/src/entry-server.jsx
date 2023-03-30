bfa808f28d6a567d1999af80680373d0833b6e7b
jdecroock <decroockjovi@gmail.com>
2023-03-30 14:57:31 +0200

    show bug

---
+++ demo/src/entry-server.jsx
@@ -0,0 +1,25 @@
+import { renderToPipeableStream } from '../../src/stream-node';
+import { App } from './App';
+
+export function render({ res, head }) {
+	res.socket.on('error', (error) => {
+		console.error('Fatal', error);
+	});
+	const { pipe } = renderToPipeableStream(<App head={head} />, {
+		onShellReady() {
+			res.statusCode = 200;
+			res.setHeader('Content-type', 'text/html');
+			pipe(res);
+		},
+		onErrorShell(error) {
+			res.statusCode = 500;
+			res.send(
+				`<!doctype html><p>An error ocurred:</p><pre>${error.message}</pre>`
+			);
+		}
+	});
+
+	// Abandon and switch to client rendering if enough time passes.
+	// Try lowering this to see the client recover.
+	// setTimeout(abort, 10000);
+}
