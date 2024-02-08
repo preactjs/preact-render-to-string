9756cc88ed46bd48efc915dbd99cd0b97ec01544
jdecroock <decroockjovi@gmail.com>
2024-02-08 13:56:27 +0100

    add a possible promise string as the return value

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,7 +1,19 @@
 import { VNode } from 'preact';
 
-export default function renderToString<P = {}>(vnode: VNode<P>, context?: any): string;
+export default function renderToString<P = {}>(
+	vnode: VNode<P>,
+	context?: any
+): string | Promise<string>;
 
-export function render<P = {}>(vnode: VNode<P>, context?: any): string;
-export function renderToString<P = {}>(vnode: VNode<P>, context?: any): string;
-export function renderToStaticMarkup<P = {}>(vnode: VNode<P>, context?: any): string;
+export function render<P = {}>(
+	vnode: VNode<P>,
+	context?: any
+): string | Promise<string>;
+export function renderToString<P = {}>(
+	vnode: VNode<P>,
+	context?: any
+): string | Promise<string>;
+export function renderToStaticMarkup<P = {}>(
+	vnode: VNode<P>,
+	context?: any
+): string | Promise<string>;
