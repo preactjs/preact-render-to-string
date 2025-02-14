3dd80bc162e5bebaf8898f3eb075d617cc4c9e79
Jovi De Croock <decroockjovi@gmail.com>
2025-02-14 20:06:11 +0100

    Move test to compat for v11 (#419)

--- test/compat/index.test.jsx
+++ test/compat/index.test.jsx
@@ -1,12 +1,58 @@
 import render from '../../src/index.js';
-import { createElement } from 'preact/compat';
+import { createElement as h, Component } from 'preact/compat';
 import { expect } from 'chai';
 
 describe('compat', () => {
 	it('should not duplicate class attribute when className is empty', async () => {
-		let rendered = render(createElement('div', { className: '' }));
+		let rendered = render(h('div', { className: '' }));
 		let expected = `<div></div>`;
 
 		expect(rendered).to.equal(expected);
+	});
+
+	it('should apply defaultProps to class components', () => {
+		class Test extends Component {
+			render(props) {
+				return <div {...props} />;
+			}
+		}
+		Test.defaultProps = {
+			foo: 'default foo',
+			bar: 'default bar'
+		};
+
+		expect(render(<Test />), 'defaults').to.equal(
+			'<div foo="default foo" bar="default bar"></div>'
+		);
+		expect(render(<Test bar="b" />), 'partial').to.equal(
+			'<div bar="b" foo="default foo"></div>'
+		);
+		expect(render(<Test foo="a" bar="b" />), 'overridden').to.equal(
+			'<div foo="a" bar="b"></div>'
+		);
+		expect(render(<Test foo={undefined} bar="b" />), 'overridden').to.equal(
+			'<div foo="default foo" bar="b"></div>'
+		);
+	});
+
+	it('should apply defaultProps to functional components', () => {
+		const Test = (props) => <div {...props} />;
+		Test.defaultProps = {
+			foo: 'default foo',
+			bar: 'default bar'
+		};
+
+		expect(render(<Test />), 'defaults').to.equal(
+			'<div foo="default foo" bar="default bar"></div>'
+		);
+		expect(render(<Test bar="b" />), 'partial').to.equal(
+			'<div bar="b" foo="default foo"></div>'
+		);
+		expect(render(<Test foo="a" bar="b" />), 'overridden').to.equal(
+			'<div foo="a" bar="b"></div>'
+		);
+		expect(render(<Test foo={undefined} bar="b" />), 'overridden').to.equal(
+			'<div foo="default foo" bar="b"></div>'
+		);
 	});
 });
