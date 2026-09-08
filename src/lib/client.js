9e6e8b16bc00f646d6df6ce75ee8797c62362691
Maxwell Cohen <to.email.max@gmail.com>
2026-09-08 11:20:24 -0400

    feat(dpu): DPU out of order streaming replacement based html end stamp for streaming

--- src/lib/client.js
+++ src/lib/client.js
@@ -2,92 +2,87 @@ import { encodeEntities } from './util.js';
 
 /* eslint-disable no-var, key-spacing, object-curly-spacing, prefer-arrow-callback, semi, keyword-spacing */
 
-// function initPreactIslandElement() {
-// 	class PreactIslandElement extends HTMLElement {
-// 		connectedCallback() {
-// 			var d = this;
-// 			if (!d.isConnected) return;
-
-// 			let i = this.getAttribute('data-target');
-// 			if (!i) return;
-
-// 			var s,
-// 				e,
-// 				c = document.createNodeIterator(document, 128);
-// 			while (c.nextNode()) {
-// 				let n = c.referenceNode;
-
-// 				if (n.data == '$s:' + i) s = n;
-// 				else if (n.data == '/$s:' + i) e = n;
-// 				if (s && e) break;
-// 			}
-// 			if (s && e && s.parentNode !== document) {
-// 				requestAnimationFrame(() => {
-// 					// Hydration may finish after connectedCallback queues this frame.
-// 					if (!d.isConnected || !s.isConnected || !e.isConnected || s.parentNode !== e.parentNode) {
-// 						d.remove();
-// 						return;
+// ((d) => {
+// 	new MutationObserver((mutations, observer) => {
+// 		for (let mutation of mutations) {
+// 			for (let node of mutation.addedNodes) {
+// 				// find all complete stamps in the DOM
+// 				if (node.nodeType == 8 && !node.data.indexOf("$p:")) {
+// 					let id = node.data.slice(3), // id of the template to replace
+// 					start, // start of the comment
+// 					end, // end of the comment
+// 					temp, // temp variable for the current node
+// 					iter = d.createTreeWalker(d, 128), // tree walker to find the start and end of the comments
+// 					hole = "$s:" + id, // start name for the comment to replace
+// 					next, // temp variable for the next item
+// 					nodes = [],
+// 					root,
+// 					owned = new Set(),
+// 					keep;
+// 					// find the start and end of the comments by traversing the tree
+// 					for (; (!start || !end) && (temp = iter.nextNode()); ) {
+//                       	if(temp.data == hole) start = temp;
+//                       	else if(temp.data == "/" + hole) end = temp;
 // 					}
-// 					var nodes = [], p = s.nextSibling, root = s.parentNode;
-// 					while (p && p !== e) {
-// 						nodes.push(p);
-// 						p = p.nextSibling;
-// 					}
-// 					if (!p) { d.remove(); return; }
-// 					// DOM already referenced by a rendered host/text VNode belongs to
-// 					// the client. A suspended component's fallback pointer does not.
-// 					while (root && !root.__k) root = root.parentNode;
-// 					var owned = new Set();
-// 					function visit(vnode) {
-// 						if (vnode) {
-// 							if (typeof vnode.type !== 'function' && vnode.__e && vnode.__e.parentNode === s.parentNode) owned.add(vnode.__e);
-// 							if (vnode.__k) vnode.__k.forEach(visit);
+// 					temp = d.querySelector('template[for="' + id + '"]');
+// 					// if the start and end are found continue
+// 					if (start && end && start.parentNode !== d && start.isConnected && end.isConnected && start.parentNode === end.parentNode) {
+// 						root = start.parentNode;
+// 						next = start.nextSibling;
+// 						while (next && next !== end) {
+// 							nodes.push(next);
+// 							next = next.nextSibling;
+// 						}
+// 						if (next) {
+// 							// DOM already referenced by a rendered host/text VNode belongs to
+// 							// the client. A suspended component's fallback pointer does not.
+// 							while (root && !root.__k) root = root.parentNode;
+// 							function visit(vnode) {
+// 								if (vnode) {
+// 									if (typeof vnode.type !== 'function' && vnode.__e && vnode.__e.parentNode === start.parentNode) owned.add(vnode.__e);
+// 									if (vnode.__k) vnode.__k.forEach(visit);
+// 								}
+// 							}
+// 							if (root) visit(root.__k);
+// 							keep = nodes.some(n => owned.has(n));
+// 							if (keep) {
+// 								let depth = 0;
+// 								nodes.forEach(n => {
+// 									if (n.nodeType === 8 && n.data.startsWith('$s')) depth++;
+// 									else if (n.nodeType === 8 && n.data.startsWith('/$s')) depth--;
+// 									else if (!depth && !owned.has(n)) n.remove();
+// 								});
+// 							} else if (temp) {
+// 								// Template is still in the document: native DPU did not apply it.
+// 								// Replace the fallback between the markers with the streamed content.
+// 								for (; (next = start.nextSibling) && next != end; ) {
+// 									next.remove();
+// 								}
+// 								start.after(temp.content);
+// 							}
 // 						}
 // 					}
-// 					if (root) visit(root.__k);
-// 					if (nodes.some(node => owned.has(node))) {
-// 						var depth = 0;
-// 						nodes.forEach(node => {
-// 							if (node.nodeType === 8 && node.data.startsWith('$s')) depth++;
-// 							else if (node.nodeType === 8 && node.data.startsWith('/$s')) depth--;
-// 							else if (!depth && !owned.has(node)) node.remove();
-// 						});
-// 						d.remove();
-// 						return;
-// 					}
-// 					p = e.previousSibling;
-// 					while (p != s) {
-// 						if (!p || p == s) break;
-// 						e.parentNode.removeChild(p);
-// 						p = e.previousSibling;
-// 					}
-
-// 					c = s;
-// 					while (d.firstChild) {
-// 						s = d.firstChild;
-// 						d.removeChild(s);
-// 						c.after(s);
-// 						c = s;
-// 					}
-
-// 					d.parentNode.removeChild(d);
-// 				});
-// 			} else d.remove();
+// 					if (temp) temp.remove();
+// 					node.remove();
+// 					// Reparse svg/math so HTML-namespace children (from template/DPU) get the right NS.
+// 					// closest() matches the element itself when the hole's parent is <svg>/<math>.
+// 					if (next && !keep) (temp = start.parentElement) && (temp = temp.closest("svg,math")) && (temp.innerHTML += "");
+// 				}
+// 			}
 // 		}
-// 	}
-
-// 	customElements.define('preact-island', PreactIslandElement);
-// }
+// 		d.readyState[0] != "l" && observer.disconnect();
+// 	}).observe(d, { childList: 1, subtree: 1 });
+// })(document);
 
 // To modify the INIT_SCRIPT, uncomment the above code, modify it, and paste it into https://try.terser.org/.
-const INIT_SCRIPT = `class e extends HTMLElement{connectedCallback(){var e=this;if(!e.isConnected)return;let t=this.getAttribute("data-target");if(t){for(var r,o,n=document.createNodeIterator(document,128);n.nextNode();){let e=n.referenceNode;if(e.data=="$s:"+t?r=e:e.data=="/$s:"+t&&(o=e),r&&o)break}r&&o&&r.parentNode!==document?requestAnimationFrame(()=>{if(e.isConnected&&r.isConnected&&o.isConnected&&r.parentNode===o.parentNode){for(var t=[],a=r.nextSibling,i=r.parentNode;a&&a!==o;)t.push(a),a=a.nextSibling;if(a){for(;i&&!i.__k;)i=i.parentNode;var d=new Set;if(i&&function e(t){t&&("function"!=typeof t.type&&t.__e&&t.__e.parentNode===r.parentNode&&d.add(t.__e),t.__k&&t.__k.forEach(e))}(i.__k),t.some(e=>d.has(e))){var s=0;return t.forEach(e=>{8===e.nodeType&&e.data.startsWith("$s")?s++:8===e.nodeType&&e.data.startsWith("/$s")?s--:s||d.has(e)||e.remove()}),void e.remove()}for(a=o.previousSibling;a!=r&&a&&a!=r;)o.parentNode.removeChild(a),a=o.previousSibling;for(n=r;e.firstChild;)r=e.firstChild,e.removeChild(r),n.after(r),n=r;e.parentNode.removeChild(e)}else e.remove()}else e.remove()}):e.remove()}}}customElements.define("preact-island",e);`;
+const INIT_SCRIPT = `(e=>{new MutationObserver((t,o)=>{for(let a of t)for(let r of a.addedNodes)if(8==r.nodeType&&!r.data.indexOf("$p:")){let d,i,s,f,l,p,c=r.data.slice(3),_=e.createTreeWalker(e,128),h="$s:"+c,m=[],N=new Set;for(;(!d||!i)&&(s=_.nextNode());)s.data==h?d=s:s.data=="/"+h&&(i=s);if(s=e.querySelector('template[for="'+c+'"]'),d&&i&&d.parentNode!==e&&d.isConnected&&i.isConnected&&d.parentNode===i.parentNode){for(l=d.parentNode,f=d.nextSibling;f&&f!==i;)m.push(f),f=f.nextSibling;if(f){for(;l&&!l.__k;)l=l.parentNode;function n(e){e&&("function"!=typeof e.type&&e.__e&&e.__e.parentNode===d.parentNode&&N.add(e.__e),e.__k&&e.__k.forEach(n))}if(l&&n(l.__k),p=m.some(e=>N.has(e)),p){let u=0;m.forEach(e=>{8===e.nodeType&&e.data.startsWith("$s")?u++:8===e.nodeType&&e.data.startsWith("/$s")?u--:u||N.has(e)||e.remove()})}else if(s){for(;(f=d.nextSibling)&&f!=i;)f.remove();d.after(s.content)}}}s&&s.remove(),r.remove(),f&&!p&&(s=d.parentElement)&&(s=s.closest("svg,math"))&&(s.innerHTML+="")}"l"!=e.readyState[0]&&o.disconnect()}).observe(e,{childList:1,subtree:1})})(document);`;
 
 /**
  * @param {string} nonce
  * @returns {string}
  */
 export function createInitScript(nonce) {
-	return `<script${nonce ? ` nonce="${encodeEntities(nonce)}"` : ''}>(function(){${INIT_SCRIPT}}())</script>`;
+	return `<script${nonce ? ` nonce="${encodeEntities(nonce)}"` : ''}>!function(){${INIT_SCRIPT}}();</script>`;
 }
 
 /**
@@ -96,5 +91,5 @@ export function createInitScript(nonce) {
  * @returns {string}
  */
 export function createSubtree(id, content) {
-	return `<preact-island hidden data-target="${id}">${content}</preact-island>`;
+	return `<template for="${id}">${content}</template><!--$p:${id}-->`;
 }
